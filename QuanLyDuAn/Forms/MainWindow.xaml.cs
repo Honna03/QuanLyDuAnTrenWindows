27eb78b65454053d5401db25c4b1c4187ab0511a
honna <huynhvanbao17203@gmail.com>
2025-03-27 21:24:13 +0700

    update

---
+++ QuanLyDuAn/Forms/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using QuanLyDuAn.Forms;
+
+namespace QuanLyDuAn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string PlaceholderText = "Tìm kiếm...";
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = new TrangChu();
+            // Khởi tạo timer để cập nhật giờ
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // Cập nhật mỗi giây
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            // Cập nhật giờ ngay khi khởi động
+            UpdateCurrentTime();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+        }
+
+        private void UpdateCurrentTime()
+        {
+            CurrentTime.Text = DateTime.Now.ToString("HH:mm:ss"); // Định dạng giờ:phút:giây
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == PlaceholderText)
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = Brushes.White; // Đổi màu chữ khi người dùng nhập
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = PlaceholderText;
+                textBox.Foreground = Brushes.Gray; // Đặt lại màu chữ placeholder
+            }
+        }
+    }
+}
